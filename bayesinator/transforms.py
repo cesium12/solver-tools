125987862fc7866f1a86c19b2db4256750f51053
rspeer <rspeer@980b859b-6828-41c1-8d2a-c5df2ce3e79e>
2008-12-17 21:37:07 +0000

    change to positive weights
    git-svn-id: https://svn.manicsages.org/r/solver-tools/trunk@46 980b859b-6828-41c1-8d2a-c5df2ce3e79e

--- bayesinator/transforms.py
+++ bayesinator/transforms.py
@@ -10,7 +10,7 @@ def trans_take_first_letters(puzzle):
         if not isinstance(s, str) or len(s) < 1:
             return []
         foo = foo + s[0]
-    return [(-2,foo)]
+    return [(2,foo)]
 
 @solver.register_transform
 def trans_take_second_letters(puzzle):
@@ -21,20 +21,20 @@ def trans_take_second_letters(puzzle):
         if not isinstance(s, str) or len(s) < 2:
             return []
         foo = foo + s[1]
-    return [(-10,foo)]
+    return [(10,foo)]
 
 
 @solver.register_transform
 def trans_sort(puzzle):
     if isinstance(puzzle, list):
-        return [(-2, sorted(puzzle))]
+        return [(2, sorted(puzzle))]
     return []
 
 @solver.register_transform
 def trans_sort_by_length(puzzle):
     if not isinstance(puzzle, list):
         return []
-    return [(-4, [b for (a,b) in sorted([(len(s),s) for s in puzzle])])]
+    return [(4, [b for (a,b) in sorted([(len(s),s) for s in puzzle])])]
 
 
 @solver.register_transform
@@ -48,10 +48,12 @@ def trans_diagonalize(puzzle):
         if len(puzzle[i]) <= i:
             return []
         foo = foo + puzzle[i][i]
-    return [(-2.5, foo)]
+    return [(2, foo)]
 
 @solver.register_transform
 def trans_reverse(puzzle):
     if not isinstance(puzzle, list):
         return []
-    return [(-3, reversed(puzzle))]
+    return [(3, reversed(puzzle))]
+
+
