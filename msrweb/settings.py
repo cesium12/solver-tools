90b267d44c8f6da8c5b6a6a43955f324fd5a256f
aisen <aisen@980b859b-6828-41c1-8d2a-c5df2ce3e79e>
2009-01-05 21:06:15 +0000

    fix template path
    git-svn-id: https://svn.manicsages.org/r/solver-tools/trunk@53 980b859b-6828-41c1-8d2a-c5df2ce3e79e

--- msrweb/settings.py
+++ msrweb/settings.py
@@ -1,5 +1,8 @@
 # Django settings for msrweb project.
 
+import os.path
+_path = os.path.dirname(__file__)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -69,7 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'templates/'
+    os.path.join(_path,'templates')
 )
 
 INSTALLED_APPS = (
