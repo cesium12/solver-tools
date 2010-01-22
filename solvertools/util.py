b27a3bdb81c9ff0fb028ae34aa2c5912216e2149
rspeer <rspeer@980b859b-6828-41c1-8d2a-c5df2ce3e79e>
2010-01-22 08:35:48 +0000

    fix a bug in loading data if the package base is the current directory
    git-svn-id: https://svn.manicsages.org/r/solver-tools/trunk@71 980b859b-6828-41c1-8d2a-c5df2ce3e79e

--- solvertools/util.py
+++ solvertools/util.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+def build_path(parts):
+    return os.path.sep.join(p for p in parts if p)
+
 def module_path():
     "Figure out the full path of this file."
     return os.path.dirname(unicode(__file__, sys.getfilesystemencoding( )))
@@ -9,9 +12,9 @@ package_dir = os.path.dirname(module_path())
 
 def get_datafile(path):
     "Get a file from the data directory."
-    return os.path.sep.join([package_dir, 'data', path])
+    return build_path([package_dir, 'data', path])
 
 def get_dictfile(path):
     "Get a file from the data/dict directory."
-    return os.path.sep.join([package_dir, 'data', 'dict', path])
+    return build_path([package_dir, 'data', 'dict', path])
 
