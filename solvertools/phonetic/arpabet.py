1403146889defc0a6522c68e890e9c88d7434540
Rob Speer <rspeer@mit.edu>
2010-11-01 19:49:01 -0400

    Deduplicated the "arpabet" code which I wrote twice.

---
+++ solvertools/phonetic/arpabet.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+This module is meant for dealing with "Arpabet" phonetic spelling, particularly
+converting it to approximate IPA so you don't have to understand its
+frustratingly misleading vowels.
+
+This uses only a subset of the IPA vowels that appear in English, and in doing
+so, eliminates many dialect distinctions. IPA [ɑ] and similar vowels are all
+represented as the plain old Roman letter "a", for example.
+
+Other simplifications: Vowel length is not represented. r's are separated from
+the vowels (sorry, Brits) and written right-side-up (sorry, linguists). A few
+different central vowels are all represented as "ə" (even though only the
+unaccented ones can actually be schwas).
+
+This actually does not lose much information given Arpabet input. Most similar vowels are already smashed together anyway. On top of that, if you're using the CMU dictionary, their words have what seems to be a Pittsburgh accent.
+
+The nine monophthong vowels in this representation are (following a sort of continuum):
+
+    u ʊ o ə a æ e ɪ i
+
+When the input contains stress notation, primary stress is indicated with an
+apostrophe before the vowel.
+"""
+IPA_VOWELS = u'uʊoaæəeɪi'
+
+ARPA_TO_IPA = {
+   'AA': u'a',
+   'AE': u'æ',
+   'AH': u'ə',
+   'AW': u'aʊ',
+   'AY': u'aɪ',
+   'EH': u'e',
+   'ER': u'ər',
+   'EY': u'eɪ',
+   'IH': u'ɪ',
+   'IY': u'i',
+   'AO': u'o',
+   'OW': u'oʊ',
+   'OY': u'oɪ',
+   'UH': u'ʊ',
+   'UW': u'u',
+   'W': u'w',
+   'Y': u'j',
+   
+   'B': u'b',
+   'CH': u'tʃ',
+   'D': u'd',
+   'DH': u'ð',
+   'F': u'f',
+   'G': u'g',
+   'HH': u'h',
+   'JH': u'dʒ',
+   'K': u'k',
+   'L': u'l',
+   'M': u'm',
+   'N': u'n',
+   'NG': u'ŋ',
+   'P': u'p',
+   'R': u'r',
+   'S': u's',
+   'SH': u'ʃ',
+   'T': u't',
+   'TH': u'θ',
+   'V': u'v',
+   'Z': u'z',
+   'ZH': u'ʒ',
+}
+
+def arpa_symbol_to_ipa(s):
+    if s[-1] in string.digits:
+        phon = CMU_PHONEMES[s[:-1]]
+        if s[-1] == '1':
+            return u"'" + phon
+        else:
+            return phon
+    else:
+        return CMU_PHONEMES[s]
+
+def arpa_to_ipa(s):
+    phonemes = s.split()
+    return u''.join(arpa_symbol_to_ipa(phon) for phon in phonemes)
+
--- solvertools/scripts/cmudict.py
+++ solvertools/scripts/cmudict.py
@@ -2,85 +2,15 @@
 import string
 import codecs
 from solvertools.util import get_dictfile
+from solvertools.phonetic.arpabet import arpa_to_ipa
 from collections import defaultdict
-
-# Note: These vowel equivalents are linguistically inaccurate, lumping together
-# many sounds that are actually different but sound basically the same to
-# most American English speakers. That's fine, because Mystery Hunt puzzle
-# writers do exactly the same thing.
-#
-# Also, we basically have to do this, because CMU already smashed together
-# things that are the same when pronounced with a Pittsburgh accent, or just
-# guessed wrong about some pronunciations.
-#
-# Anyway. This representation pretends that there are the following nine
-# monophthongs in English (following a sort of continuum):
-#
-# u ʊ o ə a æ e ɪ i
-
-CMU_PHONEMES = {
-   'AA': u'a',
-   'AE': u'æ',
-   'AH': u'ə',
-   'AW': u'aʊ',
-   'AY': u'aɪ',
-   'EH': u'e',
-   'ER': u'ər',
-   'EY': u'eɪ',
-   'IH': u'ɪ',
-   'IY': u'i',
-   'AO': u'o',
-   'OW': u'oʊ',
-   'OY': u'oɪ',
-   'UH': u'ʊ',
-   'UW': u'u',
-   'W': u'w',
-   'Y': u'j',
-   
-   'B': u'b',
-   'CH': u'tʃ',
-   'D': u'd',
-   'DH': u'ð',
-   'F': u'f',
-   'G': u'g',
-   'HH': u'h',
-   'JH': u'dʒ',
-   'K': u'k',
-   'L': u'l',
-   'M': u'm',
-   'N': u'n',
-   'NG': u'ŋ',
-   'P': u'p',
-   'R': u'r',
-   'S': u's',
-   'SH': u'ʃ',
-   'T': u't',
-   'TH': u'θ',
-   'V': u'v',
-   'Z': u'z',
-   'ZH': u'ʒ',
-}
-
-def translate_cmu_phoneme(s):
-    if s[-1] in string.digits:
-        phon = CMU_PHONEMES[s[:-1]]
-        if s[-1] == '1':
-            return u"'" + phon
-        else:
-            return phon
-    else:
-        return CMU_PHONEMES[s]
-
-def translate_cmu_phonetics(s):
-    phonemes = s.split()
-    return u''.join(translate_cmu_phoneme(phon) for phon in phonemes)
 
 def translate_cmu_entry(entry):
     text, phonemes = entry.split('  ')
     if len(text) > 3 and text[-3] == '(' and text[-1] == ')':
         # secondary entry
         text = text[:-3]
-    phonetic = translate_cmu_phonetics(phonemes)
+    phonetic = arpa_to_ipa(phonemes)
     text = text.replace('-', '')
     return text, phonetic
 
