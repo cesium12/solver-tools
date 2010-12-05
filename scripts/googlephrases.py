4783b28c61def556536bf79717796539c83e2a68
Rob Speer <rspeer@mit.edu>
2010-12-05 00:47:31 -0500

    experimenting with a new version of googlephrases script -- doesn't work yet

--- scripts/googlephrases.py
+++ scripts/googlephrases.py
@@ -1,13 +1,19 @@
 # A quick hacky script to generate google_phrases.txt from Alex's format.
 from solvertools.util import get_datafile, get_dictfile
+from solvertools.wordlist import Google200K
 import codecs
 
 thedict = {}
-for line in codecs.open(get_datafile('inputs/common_ngrams.txt'), encoding='utf-8'):
+for line in codecs.open(get_datafile('inputs/2grams.txt'), encoding='utf-8'):
     if line.strip():
         listything, freq = eval(line.strip())
-        word = ' '.join(listything)
-        thedict[word] = freq
+        expected_freq = 1e-10
+        for word in listything:
+            expected_freq *= (Google200K.get(word, 0))
+        phrase = ' '.join(listything)
+        print phrase, freq/expected_freq
+        if freq > expected_freq:
+            thedict[phrase] = freq/expected_freq
 
 words = sorted(thedict.keys())
 
