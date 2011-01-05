b8d9996aa3f94649da513e68660f8e1c077a79d6
Rob Speer <rspeer@mit.edu>
2011-01-05 03:20:36 -0500

    a better assoc function, but it'll have to wait until the database is done building

--- solvertools/puzzlebase/assoc.py
+++ solvertools/puzzlebase/assoc.py
@@ -1,45 +1,33 @@
 from solvertools.wordlist import alphanumeric_only
 from solvertools.puzzlebase.mongo import DB
-from math import log
-MIN = -1000000.0
+from math import log, exp
+from collections import defaultdict
 
-def relation_goodness(rel, word1, word2):
-    query = DB.relations.find_one(
-      {'rel': rel,
-       'words': [word1, word2]}
-    )
-    if not query:
-        return MIN
-    log_numer = log(query['freq'])
-    log_denom = 0.0
-    for word in (word1, word2):
-        key = rel+' '+word
-        query = db.totals.find_one({'_id': key})
-        if not query:
-            return MIN
-        log_denom += log(query['total'])
-    return log_numer - log_denom
-
-def associations(words, n=10, beam=50):
+def associations(words, n=10, beam=50, log_min=-20, logscale=False):
     possibilities = set()
+    if logscale:
+        minimum = log_min
+    else:
+        minimum = exp(log_min)
+    mapping = defaultdict(lambda: defaultdict(lambda: minimum))
     words = [alphanumeric_only(w) for w in words]
     for word in words:
-        query = DB.relations.find(
-          {'rel': {'$in': ['can_adjoin', 'clued_by', 'bigram']},
-           'words': word}
-        )
+        query = DB.relations.find({'words': word})
         for match in query[:beam]:
             for word2 in match['words']:
                 if word != word2 and word2 not in possibilities:
                     possibilities.add(word2)
+                    value = match.get('interestingness')
+                    if value is None:
+                        # interestingness hasn't yet been set on this
+                        # relation, so guess
+                        value = minimum/2
+                    if not logscale:
+                        value = exp(value)
+                    mapping[word2][word] = max(mapping[word2][word], value)
+
     results = {}
     for word2 in possibilities:
-        match_goodness = 0.0
-        for word in words:
-            best = 0.0
-            for rel in ['can_adjoin', 'clued_by', 'bigram']:
-                best = max(best, relation_goodness(rel, word, word2))
-            match_goodness += best
-        results[word2] = match_goodness
+        results[word2] = sum([mapping[word2][word] for word in words])
     best_results = sorted(results.items(), key=lambda x: -x[1])
     return best_results[:n]
