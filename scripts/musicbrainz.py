d465108b631f5ca2a9269ca540278a4ae985e4bf
Daniel Gulotta <dgulotta@alum.mit.edu>
2011-12-26 00:37:11 -0500

    update musicbrainz.py to use new database schema

--- scripts/musicbrainz.py
+++ scripts/musicbrainz.py
@@ -2,11 +2,11 @@
 A script for generating lists of music artists, albums, and tracks from the Musicbrainz
 database.
 
-The database can be downloaded from http://musicbrainz.org/doc/Database_Download.  Both
-the core and derived data are required.
+The database can be downloaded from http://musicbrainz.org/doc/Database_Download.  Only
+the core data is needed.
 
 The database schema can be found at:
-http://svn.musicbrainz.org/mb_server/branches/RELEASE_20090524-BRANCH/admin/sql/CreateTables.sql
+http://git.musicbrainz.org/gitweb/?p=musicbrainz-server.git;a=blob;f=admin/sql/CreateTables.sql
 """
 
 # TODO: sort output, better ranking for artists
@@ -28,124 +28,155 @@ def strip(s):
     s = s.strip()
     return s
 
-def do_albums_count():
+def lazy_readline(filename):
+    with open(filename,'r') as f:
+        s = f.readline()
+        while(s):
+            yield s.split('\t')
+            s = f.readline()
 
-    names = {}
+def do_albums_count():
+   
+    release_name = {}
+    release_group = {}
+    release = {}
+    medium = {}
     counts = {}
 
-    with open(CORE_PATH+'/album','r') as album:
-        s = album.readline()
-        while(s):
-            ss = s.split('\t')
-            uid = int(ss[0])
-            name = strip(ss[2])
-            names[uid] = name
-            s = album.readline()
-    
-    
-    with open(DERIVED_PATH+'/albummeta','r') as albummeta:
-        s = albummeta.readline()
-        while(s):
-            ss = s.split('\t')
-            try:
-                uid = int(ss[0])
-                name = names[uid]
-                count = (int(ss[3])*10)/int(ss[1])
-                counts[name] = counts.get(name,0) + count
-            except ZeroDivisionError:
-                pass
-            s = albummeta.readline()
+    for ss in lazy_readline(CORE_PATH+'/release_name'):
+        rid = int(ss[0])
+        name = strip(ss[1])
+        release_name[rid] = name
 
-    with sys.stdout as album_out:
-        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=18]
-        items.sort(key=lambda x: -x[1])
-        for name, count in items:
-            album_out.write('%s,%d\n'%(name,count))
+    for ss in lazy_readline(CORE_PATH+'/release_group'):
+        try:
+            rt = int(ss[4])
+            if rt in [1,3,4,5,9]:
+                rgid = int(ss[0])
+                rid = int(ss[2])
+                release_group[rgid]=release_name[rid]
+        except ValueError:
+            pass
+
+    del release_name
+
+    for ss in lazy_readline(CORE_PATH+'/release'):
+        rgid = int(ss[4])
+        rid = int(ss[0])
+        try:
+            release[rid]=release_group[rgid]
+        except KeyError:
+            pass
+
+    del release_group
+
+    for ss in lazy_readline(CORE_PATH+'/medium'):
+        rid = int(ss[2])
+        mid = int(ss[0])
+        try:
+            medium[mid] = release[rid]
+        except KeyError:
+            pass
+
+    del release
+
+    for ss in lazy_readline(CORE_PATH+'/medium_cdtoc'):
+        mid = int(ss[1])
+        try:
+            name = medium[mid]
+            counts[name] = counts.get(name,0)+1
+        except KeyError:
+            pass
+
+    del medium
+
+    items = [(name,count) for (name,count) in counts.iteritems() if name and count>=2]
+    items.sort(key=lambda x: -x[1])
+    for name, count in items:
+        sys.stdout.write("%s,%d\n"%(name,count))
 
 def do_artists_count():
 
-    artist_id = {}
-    artist_trackid = {}
+    artist_names = {}
+    credit_names = {}
+    recording_names = {}
     counts = {}
-    
-    with open(CORE_PATH+'/artist','r') as artist:
-        s = artist.readline()
-        while(s):
-            ss = s.split('\t')
-            aid = int(ss[0])
-            name = strip(ss[1])
-            artist_id[aid] = name
-            s = artist.readline()
-  
 
-    with open(CORE_PATH+'/track','r') as track:
-        s = track.readline()
-        while(s):
-            ss = s.split('\t')
-            tid = int(ss[0])
-            aid = int(ss[1])
-            artist_trackid[tid] = artist_id[aid]
-            s = track.readline()
+    for ss in lazy_readline(CORE_PATH+'/artist_name'):
+        aid = int(ss[0])
+        name = strip(ss[1])
+        artist_names[aid] = name
 
-    with open(CORE_PATH+'/puidjoin','r') as puidjoin:
-        s = puidjoin.readline()
-        while(s):
-            ss = s.split('\t')
-            tid = int(ss[2])
-            name = artist_trackid[tid]
-            counts[name] = counts.get(name,0)+1
-            s = puidjoin.readline()
-    
-    with sys.stdout as artist_out:
-        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=20]
-        items.sort(key=lambda x: -x[1])
-        for name, count in items:
-            artist_out.write("%s,%d\n" % (name,count))
+    for ss in lazy_readline(CORE_PATH+'/artist_credit'): 
+        cid = int(ss[0])
+        aid = int(ss[1])
+        credit_names[cid] = artist_names[aid]
+
+    del artist_names
+
+    for ss in lazy_readline(CORE_PATH+'/recording'):
+        rid = int(ss[0])
+        cid = int(ss[3])
+        recording_names[rid] = credit_names[cid]
+
+    del credit_names
+
+    for ss in lazy_readline(CORE_PATH+'/recording_puid'):
+        rid = int(ss[2])
+        name = recording_names[rid]
+        counts[name]=counts.get(name,0)+1
+
+    del recording_names
+
+    items = [(name,count) for (name,count) in counts.iteritems() if name and count>=6]
+    items.sort(key=lambda x: -x[1])
+    for name, count in items:
+        sys.stdout.write("%s,%d\n"%(name,count))
 
 def do_tracks_count():
 
-    names = {}
+    track_names = {}
+    recording_names = {}
     counts = {}
 
-    with open(CORE_PATH+'/track','r') as track:
-        s = track.readline()
-        while(s):
-            ss = s.split('\t')
-            aid = int(ss[0])
-            name = strip(ss[2])
-            names[aid] = name
-            s = track.readline()
+    for ss in lazy_readline(CORE_PATH+'/track_name'):
+        nid = int(ss[0])
+        name = strip(ss[1])
+        track_names[nid] = name
 
-    with open(CORE_PATH+'/puidjoin','r') as puidjoin:
-        s = puidjoin.readline()
-        while(s):
-            ss = s.split('\t')
-            aid = int(ss[2])
-            name = names[aid]
-            counts[name] = counts.get(name,0)+1
-            s = puidjoin.readline()
-    
-    with sys.stdout as track_out:
-        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=6]
-        items.sort(key=lambda x: -x[1])
-        for name, count in items:
-            track_out.write("%s,%d\n"%(name,count))
+    for ss in lazy_readline(CORE_PATH+'/recording'):
+        rid = int(ss[0])
+        nid = int(ss[2])
+        recording_names[rid] = track_names[nid]
+
+    del track_names
+
+    for ss in lazy_readline(CORE_PATH+'/recording_puid'):
+        rid = int(ss[2])
+        name = recording_names[rid]
+        counts[name]=counts.get(name,0)+1
+
+    del recording_names
+
+    items = [(name,count) for (name,count) in counts.iteritems() if name and count>=6]
+    items.sort(key=lambda x: -x[1])
+    for name, count in items:
+        sys.stdout.write("%s,%d\n"%(name,count))
 
 def usage():
-    sys.stderr.write("usage: musicbrainz.py core_data_path [derived_data_path] list\nwhere list is either 'artists', 'albums', or 'tracks'\n")
+    sys.stderr.write("usage: musicbrainz.py core_data_path list\nwhere list is either 'artists', 'albums', or 'tracks'\n")
     sys.exit(64)
 
 if __name__ == '__main__':
-    if len(sys.argv)<3 or len(sys.argv)>4:
+    if len(sys.argv)!=3:
         usage()
     global CORE_PATH, DERIVED_PATH
     CORE_PATH=sys.argv[1]
-    DERIVED_PATH=sys.argv[-2]
-    if sys.argv[-1]=='artists':
+    if sys.argv[2]=='artists':
         do_artists_count()
-    elif sys.argv[-1]=='albums':
+    elif sys.argv[2]=='albums':
         do_albums_count()
-    elif sys.argv[-1]=='tracks':
+    elif sys.argv[2]=='tracks':
         do_tracks_count()
     else:
         usage()
