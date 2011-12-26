fdbf244fa2f5b6bec4829c847cf6c85de2917fd5
Daniel Gulotta <dgulotta@alum.mit.edu>
2011-12-25 20:31:05 -0500

    sort musicbrainz lists, improve artist rank

--- scripts/musicbrainz.py
+++ scripts/musicbrainz.py
@@ -6,7 +6,7 @@ The database can be downloaded from http://musicbrainz.org/doc/Database_Download
 the core and derived data are required.
 
 The database schema can be found at:
-http://git.musicbrainz.org/gitweb/?p=musicbrainz-server.git;a=blob;f=admin/sql/CreateTables.sql
+http://svn.musicbrainz.org/mb_server/branches/RELEASE_20090524-BRANCH/admin/sql/CreateTables.sql
 """
 
 # TODO: sort output, better ranking for artists
@@ -27,29 +27,6 @@ def strip(s):
     s = ex3.sub('',s)
     s = s.strip()
     return s
-
-def do_albums():
-
-    allowed_albums = set()
-
-    with open(DERIVED_PATH+'/albummeta','r') as albummeta:
-        s = albummeta.readline()
-        while(s):
-            ss = s.split('\t')
-            if 3*int(ss[3])>4*int(ss[1]):
-                allowed_albums.add(int(ss[0]))
-            s = albummeta.readline()
-
-    with open(CORE_PATH+'/album','r') as album:
-        with sys.stdout as album_out:
-            s = album.readline()
-            while(s):
-                ss = s.split('\t')
-                if int(ss[0]) in allowed_albums:
-                    name = strip(ss[2])
-                    album_out.write(name)
-                    album_out.write('\n')
-                s = album.readline()
 
 def do_albums_count():
 
@@ -80,38 +57,15 @@ def do_albums_count():
             s = albummeta.readline()
 
     with sys.stdout as album_out:
-        for name, count in counts.iteritems():
-            if count >= 18 and name:
-                album_out.write('%s,%d\n'%(name,count))
-
-def do_artists():
-
-    tags = {}
-
-    with open(DERIVED_PATH+'/artist_tag','r') as artist_tag:
-        s = artist_tag.readline()
-        while(s):
-            ss = s.split('\t')
-            aid = int(ss[0])
-            count = int(ss[2])
-            tags[aid] = tags.get(aid,0)+count
-            s = artist_tag.readline()
-
-    with open(CORE_PATH+'/artist','r') as artist:
-        with open('data/dict/musicbrainz_artists_u.txt','w') as artist_out:
-            s = artist.readline()
-            while(s):
-                ss = s.split('\t')
-                aid = int(ss[0])
-                if tags.get(aid,0)>=1:
-                    name = strip(ss[1])
-                    artist_out.write(name)
-                    artist_out.write('\n')
-                s = artist.readline()
+        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=18]
+        items.sort(key=lambda x: -x[1])
+        for name, count in items:
+            album_out.write('%s,%d\n'%(name,count))
 
 def do_artists_count():
 
-    names = {}
+    artist_id = {}
+    artist_trackid = {}
     counts = {}
     
     with open(CORE_PATH+'/artist','r') as artist:
@@ -120,47 +74,33 @@ def do_artists_count():
             ss = s.split('\t')
             aid = int(ss[0])
             name = strip(ss[1])
-            names[aid] = name
+            artist_id[aid] = name
             s = artist.readline()
-    
-    with open(DERIVED_PATH+'/artist_tag','r') as artist_tag:
-        s = artist_tag.readline()
+  
+
+    with open(CORE_PATH+'/track','r') as track:
+        s = track.readline()
         while(s):
             ss = s.split('\t')
-            aid = int(ss[0])
-            name = names[aid]
-            count = int(ss[2])
-            counts[name] = counts.get(name,0)+count
-            s = artist_tag.readline()
-
-    with sys.stdout as artist_out:
-        for name, count in counts.iteritems():
-            if name:
-                artist_out.write("%s,%d\n" % (name,count))
-
-def do_tracks():
-
-    puids = {}
+            tid = int(ss[0])
+            aid = int(ss[1])
+            artist_trackid[tid] = artist_id[aid]
+            s = track.readline()
 
     with open(CORE_PATH+'/puidjoin','r') as puidjoin:
         s = puidjoin.readline()
         while(s):
             ss = s.split('\t')
-            aid = int(ss[2])
-            puids[aid] = puids.get(aid,0)+1
+            tid = int(ss[2])
+            name = artist_trackid[tid]
+            counts[name] = counts.get(name,0)+1
             s = puidjoin.readline()
-
-    with open(CORE_PATH+'/track','r') as track:
-        with sys.stdout as track_out:
-            s = track.readline()
-            while(s):
-                ss = s.split('\t')
-                aid = int(ss[0])
-                if puids.get(aid,0)>=3:
-                    name = strip(ss[2])
-                    track_out.write(name)
-                    track_out.write('\n')
-                s = track.readline()
+    
+    with sys.stdout as artist_out:
+        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=20]
+        items.sort(key=lambda x: -x[1])
+        for name, count in items:
+            artist_out.write("%s,%d\n" % (name,count))
 
 def do_tracks_count():
 
@@ -186,9 +126,10 @@ def do_tracks_count():
             s = puidjoin.readline()
     
     with sys.stdout as track_out:
-        for name, count in counts.iteritems():
-            if count>=6 and name:
-                track_out.write("%s,%d\n"%(name,count))
+        items = [(name,count) for (name,count) in counts.iteritems() if name and count>=6]
+        items.sort(key=lambda x: -x[1])
+        for name, count in items:
+            track_out.write("%s,%d\n"%(name,count))
 
 def usage():
     sys.stderr.write("usage: musicbrainz.py core_data_path [derived_data_path] list\nwhere list is either 'artists', 'albums', or 'tracks'\n")
